8448545fb71774c2e54a758cff5a5ea20afc5609
arian66 <arian.nrad@gmail.com>
2024-03-16 14:48:30 +1100

    fixed test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -4,7 +4,7 @@ test("has title", async ({ page }) => {
 	await page.goto("http://localhost:5173/");
 
 	// Expect a title "to contain" a substring.
-	await expect(page).toHaveTitle(/Playwright/);
+	await expect(page).toHaveTitle("Vite + React + TS");
 });
 
 test("get started link", async ({ page }) => {
@@ -14,7 +14,5 @@ test("get started link", async ({ page }) => {
 	await page.getByRole("button", { name: "count is 0" }).click();
 
 	// Expects page to have a heading with the name of Installation.
-	await expect(
-		page.getByRole("heading", { name: "Vite + React" })
-	).toBeVisible();
+	await expect(page.getByRole("button", { name: "count is 2" })).toBeVisible();
 });
